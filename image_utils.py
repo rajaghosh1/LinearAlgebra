3e9388e6502a22f05c50ee93c6c41ead03a6c613
Raja Ghosh <rajaghosh1@users.noreply.github.com>
2021-02-07 15:19:42 -0500

    #5 : added scaling utility

--- image_utils.py
+++ image_utils.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+import cv2
 
 
 class Color(IntEnum):
@@ -14,3 +15,10 @@ def filter_out(image, excluded_colors=None):
         for color in excluded_color_set:
             copy[:, :, color] = 0
     return copy
+
+
+def scale(image, scale_factor, interpolation=cv2.INTER_AREA):
+    height, width = image.shape[0:2]
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+    return cv2.resize(image, (new_width, new_height), interpolation)
