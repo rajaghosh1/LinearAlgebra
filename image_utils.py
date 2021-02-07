47fae27b3d596eadff4a9391b59c309681e2672a
Raja Ghosh <rajaghosh1@users.noreply.github.com>
2021-02-07 15:38:22 -0500

    #5 : added stretching utility

--- image_utils.py
+++ image_utils.py
@@ -17,8 +17,15 @@ def filter_out(image, excluded_colors=None):
     return copy
 
 
-def scale(image, scale_factor, interpolation=cv2.INTER_AREA):
+def scale(image, scale_factor=1, interpolation=cv2.INTER_AREA):
+    return stretch_by_factor(image, scale_width=scale_factor, scale_height=scale_factor, interpolation=interpolation)
+
+
+def stretch_by_factor(image, scale_width=1, scale_height=1, interpolation=cv2.INTER_AREA):
+    if ((scale_width is None) or (scale_width == 1)) and ((scale_height is None) or (scale_height == 1)):
+        return image.copy()
     height, width = image.shape[0:2]
-    new_width = int(width * scale_factor)
-    new_height = int(height * scale_factor)
+    new_width = int(width * scale_width)
+    new_height = int(height * scale_height)
     return cv2.resize(image, (new_width, new_height), interpolation)
+
